bb98e2d3f72a9d38242d61b609b01f21f7b3fdbd
brimacias <95747897+brimacias@users.noreply.github.com>
2024-10-31 14:13:04 +0100

    Delete spring boot/demo directory

--- spring boot/demo/src/main/java/com/example/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package main.java.com.example.repository;  
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import main.java.com.example.demo.usuario;  
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
-    Usuario findByNombreUsuario(String nombreUsuario);
-}
