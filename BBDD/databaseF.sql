56ebd0edf2e0c078829c46e3816ea38bdfac55e7
bri_macías <brina.padilha@gmail.com>
2024-12-08 14:15:18 +0100

    cambios base de datos

--- BBDD/databaseF.sql
+++ BBDD/databaseF.sql
@@ -4,10 +4,9 @@ use FANKINGDOM;
 
 -- Creación de tablas
 
-INSERT INTO usuarios (nombreUsuario, correoElectronico, contrasenya) VALUES ('kika', 'kika@gmail.com', 'fsdfghjkl');
 rollback;
 
-select * from usuarios;
+-- select * from comunidades;
 -- 0. Tabla usuarios
 create table if not exists usuarios (
 usuarioId int auto_increment primary key,
@@ -23,6 +22,53 @@ lugarResidencia varchar(50),
 rol enum('usuario','moderador','administrador') default 'usuario'
 );
 
+create table if not exists categoriasGenerales(
+categoriaId int auto_increment primary key,
+nombreCategoria varchar(100) not null
+);
+
+
+create table if not exists comunidades(
+comunidadId int auto_increment primary key,
+nombreComunidad varchar(255) unique not null,
+categoriaGeneral int,
+descripcion text,
+moderadorId int,
+publicacion int,
+foreign key (moderadorId) references usuarios(usuarioId),
+foreign key (categoriaGeneral) references categoriasgenerales(categoriaId)
+);
+/*
+alter table comunidades add constraint fk_comunidades_publicacion foreign key (publicacion) references publicaciones(publicacionId)
+on delete cascade;
+*/
+
+create table if not exists etiquetasstandard (
+etiquetaId int auto_increment primary key,
+textoEtiqueta varchar (255));
+
+create table if not exists warning (
+warningId int auto_increment primary key,
+textoWarning varchar(255));
+
+create table if not exists publicaciones (
+    publicacionId int auto_increment primary key,
+    titulo varchar(200) not null,
+    contenido text not null,
+    tipo enum('fanfic', 'fanart', 'fanvid') not null,
+    etiqueta int,
+    warning int,
+    usuarioId int,
+    comunidadId int,
+    fecha_creacion datetime default current_timestamp,
+    foreign key (usuarioId) references usuarios(usuarioId),
+    foreign key (comunidadId) references comunidades(comunidadId),
+    foreign key (etiqueta) references etiquetasstandard(etiquetaId),
+    foreign key (warning) references warning(warningId)
+);
+
+-- select * from usuarios;
+-- drop table publicaciones;
 create table if not exists moderadores (
 comunidadId int auto_increment primary key,
 nombreComunidad varchar(255) unique not null,
@@ -31,13 +77,7 @@ moderadorId int,
 foreign key (moderadorId) references usuarios(usuarioId)
 );
 
-create table if not exists comunidades(
-comunidadId int auto_increment primary key,
-nombreComunidad varchar(255) unique not null,
-descripcion text,
-moderadorId int,
-foreign key (moderadorId) references usuarios(usuarioId)
-);
+
 
 create table if not exists Usuario_Comunidad(
 usuarioId int,
@@ -48,34 +88,11 @@ foreign key (comunidadId) references Comunidades(comunidadId)
 );
 
 
-create table if not exists publicaciones (
-    publicacionId int auto_increment primary key,
-    titulo varchar(200) not null,
-    contenido text not null,
-    tipo enum('fanfic', 'fanart', 'fanvid') not null,
-    etiquetas varchar(255),
-    usuarioId int,
-    comunidadId int,
-    fecha_creacion datetime default current_timestamp,
-    foreign key (usuarioId) references usuarios(usuarioId),
-    foreign key (comunidadId) references comunidades(comunidadId)
-);
-
-
-create table if not exists categoriasGenerales(
-categoriaId int auto_increment primary key,
-nombreCategoria varchar(100) not null,
-etiquetasStandard text,
-etiquetasPersonalizadas text,
-cantidadTrabajos int default 0
-);
-
-
 create table if not exists comentarios(
     id int auto_increment primary key,
     contenido text not null,
     usuarioId int,
-    publicacionId int,
+	publicacionId int,
     fechaCreacion datetime default current_timestamp,
     foreign key (usuarioId) references usuarios(usuarioId),
     foreign key (publicacionId) references publicaciones(publicacionId)
@@ -87,7 +104,8 @@ create table if not exists suscripciones (
     trabajoId int,
     tipoTrabajo enum('fanfic','fanart','fanvid') not null,
     fechaSuscripcion timestamp default current_timestamp,
-    foreign key (usuarioId) references usuarios(usuarioId)
+    foreign key (usuarioId) references usuarios(usuarioId),
+    foreign key (trabajoId) references publicaciones(publicacionId)
 );
 
 -- Para obtener la información de las suscripciones de un usuario
--- BBDD/insertF.sql
+++ BBDD/insertF.sql
@@ -1,5 +1,6 @@
 use fankingdom;
 
+select * from usuarios;
 -- Registros usuarios
 /*
 insert into usuarios (nombreUsuario, correoElectronico, contrasenya, fechaCreacionPerfil, textoPresentacion, fechaNacimiento) 
@@ -9,31 +10,33 @@ insert into usuarios (nombreUsuario, correoElectronico, contrasenya, fechaCreaci
 values ('kika', 'kika@gmail.com', 'Atún18', '2024-10-27', 'Le gata', '2018-03-01');
 */
 
+insert into categoriasgenerales(nombreCategoria) value('Basado en un musical.');
+
 -- Registros comunidades
 /*
 insert into comunidades (nombreComunidad, categoriaGeneral)
-values ('Velvet', 'Basado en una serie de televisión');
+values ('Velvet', 1);
 
 insert into comunidades (nombreComunidad, categoriaGeneral)
-values ('Percy Jackson', 'Basado en una saga de libros');
+values ('Percy Jackson', 2);
 
 insert into comunidades (nombreComunidad, categoriaGeneral)
-values ('Stranger Things', 'Basado en una serie de televisión');
+values ('Stranger Things', 1);
 
 insert into comunidades (nombreComunidad, categoriaGeneral)
-values ('Fantastic Beasts', 'Basado en una saga de películas');
+values ('Fantastic Beasts', 3);
 
 insert into comunidades (nombreComunidad, categoriaGeneral)
-values ('Hamilton', 'Basado en un musical');
+values ('Hamilton', 4);
 
 insert into comunidades (nombreComunidad, categoriaGeneral)
-values ('The Big Bang Theory', 'Basado en una serie de televisión');
+values ('The Big Bang Theory', 1);
 
 insert into comunidades (nombreComunidad, categoriaGeneral)
-values ('Harry Potter', 'Basado en una saga de libros');
+values ('Harry Potter', 2);
 
 insert into comunidades (nombreComunidad, categoriaGeneral)
-values ('Epic', 'Basado en un musical');
+values ('Epic', 4);
 */
 
 -- Registros usuario_Comunidad
@@ -44,37 +47,29 @@ INSERT INTO Usuario_Comunidad (usuarioId, comunidadId) VALUES (1, 4), (1, 3);
 */
 
 
--- Registros categorías generales
-/*
-insert into categoriasgenerales (nombreCategoria)
-value ('Basado en un libro');
-
-insert into categoriasgenerales (nombreCategoria)
-value ('Basado en una saga de libros');
-
-insert into categoriasgenerales (nombreCategoria)
-value ('Basado en una película');
-
-insert into categoriasgenerales (nombreCategoria)
-value ('Basado en una saga de películas');
-
-insert into categoriasgenerales (nombreCategoria)
-value ('Basado en una serie de televisión');
-
-insert into categoriasgenerales (nombreCategoria)
-value ('Basado en un musical');
-*/
-
-
 -- Consultas a tablas
+-- drop table publicaciones;
 
-select * from comunidades /*where categoriaGeneral = 'Basado en una serie de televisión'*/;
+-- select * from comunidades /*where categoriaGeneral = 'Basado en una serie de televisión'*/;
  
-describe categoriasgenerales;
+-- describe categoriasgenerales;
 
- 
+-- select * from publicaciones;
 -- Borrados de registros erróneos
 
 -- delete from comunidades where comunidadId > 8;
 
 -- delete from categoriasgenerales where categoriaId = 3;
+select * from warning;
+-- insert into etiquetasstandard(textoEtiqueta) value ('One Shot');
+-- insert into warning(textoWarning) value ('+18');
+
+select * from publicaciones;
+
+insert into publicaciones(titulo,contenido,tipo,etiqueta,warning,usuarioId,comunidadId) values ("The Hound of the Baskervilles",
+"Brian was overjoyed.
+How could he not? He has just been handpicked by Rupert Murdoch, a media mogul who had recently bought an English newspaper, to work for him. Mr. Murdoch seemed to be good friends with Richard Pickens, a legend in journalism and someone Brian was proud to call a mentor, and he had been the one to put on a good word for him and other coleagues who were going work in the newspaper with him.
+It was such an honor that of all the journalists he knew, Mr. Pickens had not only remembered, but also recommened him for such a job. He hadn’t been the only one, of course, but being among the selected group was a huge deal. Ingrid would be proud. Or maybe not so much.
+His wife had never really understood his fixation with his work. She had respected it, but could never understand why when he was working in a case, it would become the only thing in his mind and he was unable to concentrate very much in anything else. He worked a lot, and she had been left aside. She would often ask him, practically beg him, to quit his job. They had more that enough money to live comfortably for the rest of their lives. His job could be dangerous, he had been threatened for his reports before, it wasn’t fitting for a family man.
+She had really wanted to have children, and he had wanted it too, but not nearly as much as he should have, or as much as he wanted to prove that the company he had been investigating hired illegal inmigrants and had them working in inhumane conditions, or as much as he wanted to put the boss and employes who were involved behind bars.
+She was right to leave him, he wasn’t ready to truly commit to anything that didn’t have to do with his work.", 'fanfic', 3, 4, 1, 1);
